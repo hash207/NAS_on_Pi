e1d5f40040ecbce9749efd403e0d9b081ac945a1
hash207 <hashemalshar20.7@gmail.com>
2025-08-18 16:16:10 +0300

    Turn the app into a file manager that can play videos

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, redirect, url_for
 import os
 
 app = Flask(__name__)
 MEDIA_FOLDER = '/home/hashem-alsharif/Desktop/Hashem/My Work/NAS_on_Pi/media'
 
+def get_dir_contents(subpath=''):
+    abs_path = os.path.join(MEDIA_FOLDER, subpath)
+    items = []
+    for entry in os.listdir(abs_path):
+        entry_path = os.path.join(abs_path, entry)
+        rel_path = os.path.join(subpath, entry) if subpath else entry
+        if os.path.isdir(entry_path):
+            items.append({'type': 'dir', 'name': entry, 'path': rel_path})
+        elif entry.endswith('.mp4'):
+            items.append({'type': 'video', 'name': entry, 'path': rel_path})
+        else:
+            items.append({'type': 'file', 'name': entry, 'path': rel_path})
+    return items
+
 @app.route('/')
 def index():
-    files = os.listdir(MEDIA_FOLDER)
-    media_files = [f for f in files if f.endswith(('.mp4'))]
-    return render_template('index.html', media_files=media_files)
+    return redirect(url_for('browse', subpath=''))
 
-@app.route('/media/<filename>')
+@app.route('/browse/', defaults={'subpath': ''})
+@app.route('/browse/<path:subpath>')
+def browse(subpath):
+    items = get_dir_contents(subpath)
+    parent = os.path.dirname(subpath) if subpath else None
+    return render_template('index.html', items=items, current_path=subpath, parent=parent)
+
+@app.route('/media/<path:filename>')
 def stream_file(filename):
     return send_from_directory(MEDIA_FOLDER, filename)
 
