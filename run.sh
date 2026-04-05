8da2d4698bdbf4054fde8c2fc21150eee8738059
hash207 <hashemalshar20.7@gmail.com>
2026-04-05 22:53:08 +0300

    Connect Web app eoth Ngrok static IP

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 # Activate the virtual environment
-source nas_venv/bin/activate
+source /home/hashem-alsharif/Desktop/Hashem/My_Work/NAS_on_Pi/nas_venv/bin/activate
 
-# Start the Flask app 
-python3 "Web app/main.py" 
+# Start the Flask app in the background
+python3 "/home/hashem-alsharif/Desktop/Hashem/My_Work/NAS_on_Pi/Web app/main.py" &
+
+# Wait a few seconds for Flask to start
+sleep 3
+
+# Start ngrok tunnel
+python3 /home/hashem-alsharif/Desktop/Hashem/My_Work/NAS_on_Pi/any/start_ngrok.py
+
+# End working in background
+python3 /home/hashem-alsharif/Desktop/Hashem/My_Work/NAS_on_Pi/any/kill.py
